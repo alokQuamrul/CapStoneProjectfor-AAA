c4be05411c4985ca322cb3c4c68c67980259306c
alokQuamrul <islam.kamrul3105@gmail.com>
2025-07-30 20:06:03 +0600

    CapstoneProjFor-AAA

---
+++ script.js
@@ -0,0 +1,13 @@
+const slides = document.querySelector('.slides');
+const slideCount = document.querySelectorAll('.slide').length;
+let currentIndex = 0;
+
+document.querySelector('.next').addEventListener('click', () => {
+    currentIndex = (currentIndex + 1) % slideCount;
+    slides.style.transform = `translateX(-${currentIndex * 100}%)`;
+});
+
+document.querySelector('.prev').addEventListener('click', () => {
+    currentIndex = (currentIndex - 1 + slideCount) % slideCount;
+    slides.style.transform = `translateX(-${currentIndex * 100}%)`;
+});
